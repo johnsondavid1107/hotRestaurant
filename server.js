854c9a305d40e84d925296539ae7d11b6f894af1
Ness <johnsondavid1107@gmail.com>
2020-11-19 08:10:15 -0500

    fixed arrays

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var path = require("path");
 
-var PORT = 9999;
+var PORT = 3003;
 var app = express();
 
 
@@ -10,66 +10,66 @@ app.use(express.json());
 app.use(express.static(__dirname + '/assets'))
 
 
+const tables = [];
 const onTheBooks = [];
-
-const waitlist = [];
-
-for (let i = 0; i < onTheBooks.length; i++){
-
-    if (onTheBooks[i] > 5){
-        waitlist = onTheBooks.slice(5,onTheBooks.length);
-    }
-}
-
-console.log(onTheBooks);
-console.log(waitlist);
+var waitlist = [];
 
 
 
 
-
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
 
     res.sendFile(path.join(__dirname, "index.html"));
-    console.log("knocking on the Home door!")
+    // console.log("knocking on the Home door!")
 })
 
-app.get("/add", function(req, res) {
+app.get("/add", function (req, res) {
 
     res.sendFile(path.join(__dirname, "add.html"));
-    console.log("knocking on the ADD door!")
-    
+    // console.log("knocking on the ADD door!")
+
 })
 
-app.get("/reservations", function(req, res) {
+app.get("/reservations", function (req, res) {
 
     res.sendFile(path.join(__dirname, "reservations.html"));
-    console.log("knocking on the RESERVATIONS door!")
-   
+    // console.log("knocking on the RESERVATIONS door!")
+
 });
 
-app.get("/api/table", function(req, res) {
+app.get("/api/table", function (req, res) {
 
-    
-    console.log("knocking on the API Table door!")
-    return res.json(onTheBooks);
-});
-
-app.get("/api/waitlist", function(req, res) {
-
-    
-    console.log("knocking on the API WAITLIST door!")
-    return res.json(waitlist);
+if(tables.length > 5){
+    console.log("This is true");
+    waitlist = tables.slice(5,tables.length);
     console.log(waitlist);
+    console.log(tables);
+}
+
+
+    // console.log("knocking on the API Table door!")
+    return res.json(tables);
+    // console.log(tables)
+});
+
+app.get("/api/waitlist", function (req, res) {
+
+   
+    // console.log("knocking on the API WAITLIST door!")
+
+    return res.json(waitlist);
+
+
+
 });
 
 
 
-app.post("/reservation", function(req, res) {
+app.post("/reservation", function (req, res) {
 
     const reservations = req.body;
-    console.log(reservations);
-    onTheBooks.push(reservations);
+    // console.log(reservations);
+    tables.push(reservations);
 
 })
 
@@ -98,6 +98,6 @@ app.post("/reservation", function(req, res) {
 
 
 
-app.listen(PORT, function(){
+app.listen(PORT, function () {
     console.log("You did it Five!  Port open and listening on " + PORT)
 })
